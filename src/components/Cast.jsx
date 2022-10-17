8e1853e5404d31d6b20abd0cdd59d3f0338bbd27
Lakei87 <monastyrskyis@gmail.com>
2022-10-17 12:41:21 +0200

    add Cast and Reviews components

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { MoviesApiService } from "services/moviesApi";  
+
+const moviesApi = new MoviesApiService();
+const urlImg = 'https://image.tmdb.org/t/p/w500/';
+
 
 export default function Cast() {
+  const [cast, setCast] = useState([]);
+  const { id } = useParams();
+  moviesApi.ID = id;
+
+  useEffect(() => { 
+    const getCredits = async () => {
+      const responce = await moviesApi.getCredits();
+      setCast(responce.cast);
+      console.log(responce.cast);
+    }
+    getCredits();
+  }, []);
+
   return (
     <div>
-      <p>Cast</p>
+      <ul>
+        {cast.map(({id, profile_path, name, character}) => (
+          <li key={id}>
+            <img src={`${urlImg}${profile_path}`} alt={name} />
+            <p>{name}</p>
+            <p>Character: {character}</p>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { MoviesApiService } from "services/moviesApi";  
 
+const moviesApi = new MoviesApiService();
 
 export default function Reviews() {
+    const [reviews, setReviews] = useState([]);
+    const { id } = useParams();
+    moviesApi.ID = id;
+
+    useEffect(() => { 
+        const getReviews = async () => {
+        const responce = await moviesApi.getReviews();
+        setReviews(responce.results);
+        console.log(responce.results);
+        }
+        getReviews();
+    }, []);
+    
     return (
         <div>
-            <p>Reviews</p>
+            <ul>
+                {reviews.map(({ author, id, content }) => (
+                    <li key={id}>
+                        <p>{author}</p>
+                        <p>{content}</p>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
